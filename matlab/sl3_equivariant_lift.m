25d342392da4a653f97fe57054c6bfc4585a46fb
justin <tzuyuan@umich.edu>
2024-04-17 18:13:17 -0400

    testing lifting

--- matlab/sl3_equivariant_lift.m
+++ matlab/sl3_equivariant_lift.m
@@ -1,12 +1,12 @@
-clear;
 clc;
+clear; 
 
-syms n1 n2 n3 a1 a2 v1 v2 v3 v4 v5 v6 v7 v8
-assumeAlso([n1 n2 n3 a1 a2 v1 v2 v3 v4 v5 v6 v7 v8],'real')
+syms v1 v2 v3 v4 v5 v6 v7 v8
+assumeAlso([v1 v2 v3 v4 v5 v6 v7 v8],'real')
 
 syms h1 h2 h3 h4 h5 h6 h7 h8 h9
 assumeAlso([h1 h2 h3 h4 h5 h6 h7 h8 h9],'real')
-%% basis
+%%
 
 E1 = [1, 0, 0; 0, -1, 0; 0, 0, 0];
 E2 = [0, 1, 0; 1, 0, 0; 0, 0, 0];
@@ -16,6 +16,8 @@ E5 = [0, 0, 1; 0, 0, 0; 0, 0, 0];
 E6 = [0, 0, 0; 0, 0, 1; 0, 0, 0];
 E7 = [0, 0, 0; 0, 0, 0; 1, 0, 0];
 E8 = [0, 0, 0; 0, 0, 0; 0, 1, 0];
+
+E = {E1,E2,E3,E4,E5,E6,E7,E8};
 
 E1_vec = reshape(E1,1,[])';
 E2_vec = reshape(E2,1,[])';
@@ -31,25 +33,6 @@ E_vec = [E1_vec, E2_vec, E3_vec, E4_vec, E5_vec, E6_vec, E7_vec, E8_vec];
 
 x_hat = v1*E1+v2*E2+v3*E3+v4*E4+v5*E5+v6*E6+v7*E7+v8*E8;
 
-%% find Ad_N
-N = [1 n1 n2; 0 1 n3; 0 0 1];
-Ad_N_hat = N*x_hat*inv(N);
-Ad_N_hat_vec = reshape(Ad_N_hat,1,[])';
-
-% solve for least square
-x = inv(E_vec'*E_vec)*(E_vec')*Ad_N_hat_vec;
-var = [v1,v2,v3,v4,v5,v6,v7,v8];
-[Ad_N,b]=equationsToMatrix(x,var);
-
-%% find Ad_A
-A = [a1, 0,0; 0,a2,0;0,0,1/a1/a2];
-Ad_A_hat = A*x_hat*inv(A);
-Ad_A_hat_vec = reshape(Ad_A_hat,1,[])';
-
-% solve for least square
-x = inv(E_vec'*E_vec)*(E_vec')*Ad_A_hat_vec;
-var = [v1,v2,v3,v4,v5,v6,v7,v8];
-[Ad_A,b]=equationsToMatrix(x,var);
 
 %% find h
 H = [h1,h2,h3;h4,h5,h6;h7,h8,h9];
@@ -59,10 +42,39 @@ Ad_H_hat_vec = reshape(Ad_H_hat,1,[])';
 % solve for least square
 x = inv(E_vec'*E_vec)*(E_vec')*Ad_H_hat_vec;
 var = [v1,v2,v3,v4,v5,v6,v7,v8];
-[Ad_H,b]=equationsToMatrix(x,var);
+[Ad_H_sym,b]=equationsToMatrix(x,var);
 
-Ad_kron_H = kron(inv(H)',Ad_H);
+syms f(h1,h2,h3,h4,h5,h6,h7,h8,h9);
+f(h1,h2,h3,h4,h5,h6,h7,h8,h9) = Ad_H_sym;
 
-%%
-% sigma = svd(Ad_kron_H);
-% z = null(Ad_kron_H - eye(24))
+%% find Ad_Ei
+Ad_E = {};
+dAd_E = {};
+for i=1:8
+    % Find Ad_E_i
+    H = expm(E{i}); % Using numerical exponential for now
+    Ad_E{i} = double(f(H(1,1),H(1,2),H(1,3),H(2,1),H(2,2),H(2,3),H(3,1),H(3,2),H(3,3)));
+    dAd_E{i} = logm(Ad_E{i});
+end
+
+%% construct dro_3
+
+dro_3 = {};
+C = [];
+for i =1:8
+   dro_3{i} = kron(-E{i}',eye(8))+kron(eye(3),dAd_E{i});
+   C = [C;dro_3{i}];
+end
+
+%% 
+C_minus_I = C-eye(size(C));
+rank(C_minus_I)
+[U,S,V] = svd(C);
+
+
+Q = null(C);
+
+
+
+
+
--- playground/emlp_test.py
+++ playground/emlp_test.py
@@ -2,20 +2,21 @@ import torch
 
 import emlp.nn.pytorch as nn
 from emlp.reps import T,V,sparsify_basis
-from emlp.groups import SO
+from emlp.groups import SO, SL
 
 
 
 if __name__ == '__main__':
     # Define the input representation
-    G = SO(3)
+    G = SL(3)
     reps = V(G)
-    reps_out = V(G)
-
+    reps_out = (V**1*V.T**1)(G)
+    
     print(reps.rho(G))
     print(reps.size())
+    print(reps_out.size())
     Q = (reps>>reps_out).equivariant_basis() 
     print(f"Basis matrix of shape {Q.shape}")
-    print(sparsify_basis(Q).reshape(3,3))
+    # print(sparsify_basis(Q).reshape(3,3))
     print(reps(G).size())
     # Define the output representation
